ba4cecfcf4b2e6ee6981394fc5d18b7170dac53f
denisevaldivia <dianavaldiviavargas03@gmail.com>
2026-03-30 16:10:20 -0600

    [feat]: Modify architecture back to otiginal

--- src/models/Discriminator.py
+++ src/models/Discriminator.py
@@ -15,13 +15,11 @@ from torch.nn.utils import spectral_norm
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Sequential(
         nn.ReflectionPad2d(1),
-        spectral_norm(
             nn.Conv2d(in_planes, out_planes,
                       kernel_size=3,
                       stride=stride,
                       padding=0,
                       bias=False)
-        )
     )
 
 class Discriminator(nn.Module):
@@ -36,14 +34,14 @@ class Discriminator(nn.Module):
             nn.LeakyReLU(0.2, inplace=True),
 
             conv3x3(64, 128, stride=1),
-            #nn.BatchNorm2d(128),
+            nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
 
             conv3x3(128, 128, stride=2),
             nn.LeakyReLU(0.2, inplace=True),
 
             conv3x3(128, 256, stride=1),
-            #nn.BatchNorm2d(256),
+            nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
 
             conv3x3(256, 1, stride=1)  # PatchGAN output
